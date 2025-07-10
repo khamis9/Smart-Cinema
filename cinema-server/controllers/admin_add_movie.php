661cbf0f333f429903fa6af7faeee821adca7ae6
khamis9 <khamishussein2003@gmail.com>
2025-07-10 13:40:00 +0300

    fixed a bug in adding a movie

--- cinema-server/controllers/admin_add_movie.php
+++ cinema-server/controllers/admin_add_movie.php
@@ -4,15 +4,15 @@ require("../connection/connection.php");
 $response = ["status" => 200];
 $data = json_decode(file_get_contents("php://input"), true);
 
-if (!isset($data["name"], $data["trailer"], $data["cast"], $data["rating"])) {
+if (!isset($data["title"], $data["trailer_url"], $data["rating"], $data["release_date"], $data["description"])) {
     $response["status"] = 400;
     $response["message"] = "Missing movie data";
     echo json_encode($response);
     return;
 }
 
-$stmt = $mysqli->prepare("INSERT INTO movies (name, trailer, cast, rating) VALUES (?, ?, ?, ?)");
-$stmt->bind_param("ssss", $data["name"], $data["trailer"], $data["cast"], $data["rating"]);
+$stmt = $mysqli->prepare("INSERT INTO movies (title, trailer_url, rating, release_date, description) VALUES (?, ?, ?, ?, ?)");
+$stmt->bind_param("sssss", $data["title"], $data["trailer_url"], $data["rating"], $data["release_date"], $data["description"]);
 $stmt->execute();
 
 $response["message"] = "Movie added successfully";
